4048ced6f1e42abc69a1bf7f87e645e6cd738cdb
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-11-24 12:17:46 +0800

    added user management

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Authentication
 Route::post("/register", "AuthController@register");
 Route::post("/login", "AuthController@login");
+
+// User Management
+Route::resource("/users", "UserController");
+
