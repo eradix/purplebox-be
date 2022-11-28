a606cf0dcd1a6595d1984b3690179f64484ce3f0
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-11-28 12:02:34 +0800

    added store product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Image;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -12,13 +13,30 @@ class ProductController extends Controller
     {
         $products = Product::orderByDesc("id")->get();
         return response()->json([
-            "data" => $products
-        ]);
+        "data" => $products
+    ]);
     }
 
     public function store(Request $request)
     {
-        
+        if($request->hasFile('image')) {
+            $image = $request->image->store('cakes');
+            $img = Image::make(public_path('storage/' . $image))->fit(400, 500);
+            $img->save();
+        }
+
+        $product = Product::create([
+            "name" => $request->name,
+            "type" => $request->type,
+            "image" => $image,
+            "description" => $request->description,
+            "price" => $request->price,
+        ]);
+
+        return response()->json([
+            "message" => "Product has been added.",
+            "data" => $product,
+        ], 201);
     }
 
     
